36be2b57bdeba8205495748604202d8eef9fd523
tanridong <tanridong@yy.com>
2023-05-10 20:07:32 +0800

    test: 备份

--- src/components/pages/threeJS/components/reskin.tsx
+++ src/components/pages/threeJS/components/reskin.tsx
@@ -66,6 +66,7 @@ const Reskin: React.FC = () => {
 
         // Offset the y position a bit
         theModel.position.y = -1
+        theModel.rotation.y = Math.PI
 
         // Add the model to the scene
         scene.add(theModel)
@@ -76,6 +77,35 @@ const Reskin: React.FC = () => {
       }
     )
 
+    // 半球光
+    // 光源直接放置于场景之上，光照颜色从天空光线颜色渐变到地面光线颜色。
+    // 半球光不能投射阴影。
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.61)
+    hemiLight.position.set(0, 50, 0)
+    scene.add(hemiLight)
+
+    // 平行光
+    // 平行光是沿着特定方向发射的光。这种光的表现像是无限远,从它发出的光线都是平行的。常常用平行光来模拟太阳光 的效果; 太阳足够远，因此我们可以认为太阳的位置是无限远，所以我们认为从太阳发出的光线也都是平行的。
+    // 平行光可以投射阴影
+    const dirLight = new THREE.DirectionalLight(0xffffff, 0.54)
+    dirLight.position.set(-8, 12, 8)
+    dirLight.castShadow = true
+    dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024)
+    scene.add(dirLight)
+
+    // 创建地板
+    // 平面缓冲几何体
+    const floorGeometry = new THREE.PlaneGeometry(5000, 5000, 1, 1)
+    const floorMaterial = new THREE.MeshPhongMaterial({
+      color: 0xff0000,
+      shininess: 0
+    })
+    const floor = new THREE.Mesh(floorGeometry, floorMaterial)
+    floor.rotation.x = -0.5 * Math.PI
+    floor.receiveShadow = true
+    floor.position.y = -1
+    scene.add(floor)
+
     animate()
   }
 
@@ -84,6 +114,28 @@ const Reskin: React.FC = () => {
     renderer?.render(scene, camera)
 
     requestAnimationFrame(animate)
+
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement
+      camera.aspect = canvas.clientWidth / canvas.clientHeight
+      camera.updateProjectionMatrix()
+    }
+  }
+
+  const resizeRendererToDisplaySize = (renderer: THREE.Renderer) => {
+    const canvas = renderer.domElement
+    const width = window.innerWidth
+    const height = window.innerHeight
+    const canvasPixelWidth = canvas.width / window.devicePixelRatio
+    const canvasPixelHeight = canvas.height / window.devicePixelRatio
+
+    const needResize = canvasPixelWidth !== width || canvasPixelHeight !== height
+
+    if (needResize) {
+      renderer.setSize(width, height, false)
+    }
+
+    return needResize
   }
 
   useEffect(() => {
@@ -92,7 +144,7 @@ const Reskin: React.FC = () => {
 
   return (
     <>
-      <canvas id='canvas' />
+      <canvas width={window.innerWidth} height={window.innerHeight} id='canvas' />
     </>
   )
 }
